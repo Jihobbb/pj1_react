cc2d698eba137a99f5cf02287905b2a515964157
EHEH <81672416+eunhoya@users.noreply.github.com>
2022-05-09 14:46:05 +0900

    eh 1
    1

--- src/react_project/Calendar.jsx
+++ src/react_project/Calendar.jsx
@@ -4,102 +4,60 @@ import FullCalendar, { formatDate } from '@fullcalendar/react'; // must go befor
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import InputInfo from './InputInfo'
+import InputInfo from './InputInfo';
 
-
-
+import './Calendar.css';
 
 const Calendar = () => {
-
   const [modal_state, setModal_state] = useState(false);
-  const [startStr, setStartStr] = useState("");
-  const [endStr, setEndStr] = useState("");
+  const [startStr, setStartStr] = useState('');
+  const [endStr, setEndStr] = useState('');
   const [user, setUser] = useState([]);
 
-
-  let str = formatDate(new Date(), {
-    month: 'long',
-    year: 'numeric',
-    day: 'numeric',
-  });
-
-
   const getdata = async () => {
-    const response = await axios.get('http://localhost:8081/api/planList');
+    const response = await axios.get('http://localhost:8081//api/planDelete/');
 
     setUser(response.data);
   };
 
-  useEffect(() => {    
+  useEffect(() => {
     getdata();
   }, []);
-
-  // axios
-  //   .get('http://localhost:8081/api/planList')
-  //   .then((Response) => {
-  //     console.log('res', Response);
-  //   })
-  //   .catch((Error) => {
-  //     console.log('getError', Error);
-  //   });
-
-  // const handleDateSelect = (selectInfo) => {
-  //   let title = prompt('일정 :');
-  //   // title 값이 있을때, 화면에 calendar.addEvent() json형식으로 일정을 추가
-  //   let calendarApi = selectInfo.view.calendar;
-  //   calendarApi.unselect(); // clear date selection
-  //   // alert('Date: ' + selectInfo.dateStr); // 선택날짜
-  //   if (title) {
-  //     calendarApi.addEvent(
-  //       axios
-  //         .post('http://localhost:8081/api/planSave', {
-  //           id: createEventId(),
-  //           title: title,
-  //           start_time: selectInfo.dateStr,
-  //           end_time: selectInfo.dateStr,
-  //           allDay: selectInfo.allDay,
-  //         })
-  //         .then(function (res) {
-  //           // 그러면
-  //           console.log(res);
-  //         })
-  //         .catch(function (error) {
-  //           //에러
-  //           console.log('postError', error);
-  //         })
-  //     );
-  //   }
-  //   // change the day's background color just for fun
-  //   // info.dayEl.style.backgroundColor = 'red';
-  // };
-
 
   const handleDateSelect = (selectInfo) => {
     setModal_state(!modal_state);
     setStartStr(selectInfo.startStr);
     setEndStr(selectInfo.endStr);
-  }
+  };
 
   const onChange = () => {
     setModal_state(!modal_state);
-  }
-
+  };
 
   const handleEventClick = (clickInfo) => {
-    if (window.confirm(`삭제 '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove();
-    }
+    axios
+      .delete('http://localhost:8081/api/planDelete/{id}')
+      .then((response) => {
+        console.log('삭제 요청 성공');
+        if (window.confirm(`삭제 '${clickInfo.event.title}'`)) {
+          clickInfo.event.remove();
+        }
+      })
+      .catch(() => {
+        console.log('삭제 요청 실패');
+      });
+
     console.log('handleEventClick', clickInfo);
   };
 
   function renderEventContent(eventInfo) {
-  return (
-    <>
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-    </>
-  )
-}
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    );
+  }
 
   // const box = () => {
   //   user.map(user => {
@@ -111,10 +69,12 @@ const Calendar = () => {
 
   return (
     <div>
-      <InputInfo modal_state = {modal_state}
-                 startStr = {startStr}
-                 endStr = {endStr}
-                 onChange = {onChange}/>
+      <InputInfo
+        modal_state={modal_state}
+        startStr={startStr}
+        endStr={endStr}
+        onChange={onChange}
+      />
 
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -128,7 +88,11 @@ const Calendar = () => {
         }}
         initialView='timeGridWeek'
         ///////////////////////////////
-        events={user.map(user => ({ title: user.title,  start: user.start_time, end: user.end_time}))}
+        events={user.map((user) => ({
+          title: user.title,
+          start: user.start_time,
+          end: user.end_time,
+        }))}
         ////////////////////////////////
 
         // events = {{title : 'All Day',
@@ -140,7 +104,6 @@ const Calendar = () => {
         //   { title: 'event 1', date: '2022-05-11', start: '2022-05-11T08:00:00+09:00', end:'2022-05-11T11:00:00+09:00'},
         //   { title: 'event 2', date: '2022-05-10'  }
         // ]}
-
 
         eventClick={handleEventClick}
         select={handleDateSelect}
@@ -157,229 +120,3 @@ const Calendar = () => {
 };
 
 export default Calendar;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, {useEffect} from 'react'
-// import FullCalendar, { formatDate } from '@fullcalendar/react'
-// import dayGridPlugin from '@fullcalendar/daygrid'
-// import timeGridPlugin from '@fullcalendar/timegrid'
-// import interactionPlugin from '@fullcalendar/interaction'
-// import axios from 'axios'
-// import InputInfo from './InputInfo'
-// import "./Calendar.css"
-
-// axios.defaults.withCredentials = true;
-
-// export default class Calendar extends React.Component {
-//   state = {
-//     weekendsVisible: true,
-//     currentEvents: [],
-//     modal_state : false,
-//     startStr : "",
-//     endStr : "",
-//   }
-  
-  
-//   onChange = () => {
-//     this.setState ({
-//       modal_state : !this.state.modal_state
-//     })
-    
-//   }
-
-//   render() {
-//     return (
-//       <div className='demo-app'>
-//         {this.renderSidebar()}
-//         <div className='demo-app-main'>
-//           <InputInfo modal_state = {this.state.modal_state} startStr = {this.state.startStr} endStr = {this.state.endStr} onChange={this.onChange}/>
-//           <FullCalendar
-//             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-//             locale="ko"
-//             businessHours={true} // 주말 색깔 블러 처리
-//             headerToolbar={{
-//               left: 'prev',
-//               center: 'title',
-//               right: 'today dayGridMonth, timeGridWeek, next',
-              
-//             }}
-//             initialView='timeGridWeek'
-//             views={{
-//               dayGridMonth: {
-//                 titleFormat: { year: "numeric", month: "short" },
-//               },
-//               week: {
-//                 titleFormat: { year: "numeric", month: "short" },
-//               },
-//             }}
-            
-//             editable={true}
-//             selectable={true}
-//             selectMirror={true}
-//             // 이벤트명 : function(){} : 각 날짜에 대한 이벤트를 통해 처리할 내용..
-//             dayMaxEvents={true}
-//             weekends={this.state.weekendsVisible}
-//             select={this.handleDateSelect}
-//             eventContent={renderEventContent} // custom render function
-//             eventClick={this.handleEventClick}
-//             eventsSet={this.handleEvents}
-//              // called after events are initialized/added/changed/removed
-//             /* you can update a remote database when these fire:
-//             eventAdd={function(){}}
-//             eventChange={function(){}}
-//             eventRemove={function(){}}
-//             */
-//           />
-          
-//         </div>
-        
-//       </div>
-//     )
-//   }
-
-//   renderSidebar() {
-//     return (
-//       <div className='demo-app-sidebar'>
-//         <div className='demo-app-sidebar-section'>
-//           <h2>사용방법</h2>
-//           <ul>
-//             <li>날짜를 선택하면 새 이벤트를 생성하라는 메시지가 표시됩니다.</li>
-//             <li>이벤트 드래그, 드롭 및 크기 조정</li>
-//             <li>삭제할 이벤트 클릭</li>
-//           </ul>
-//         </div>
-
-//         {/* 주말 생성 제거 토글 */}
-//         <div className='demo-app-sidebar-section'>
-//           <label>
-//             <input
-//               type='checkbox'
-//               checked={this.state.weekendsVisible}
-//               onChange={this.handleWeekendsToggle}
-//             ></input>
-//             toggle weekends
-//           </label>
-//         </div>
-//         <div className='demo-app-sidebar-section'>
-//           <h2>모든 일정 ({this.state.currentEvents.length})</h2>
-//           <ul>
-//             {this.state.currentEvents.map(renderSidebarEvent)}
-//           </ul>
-//         </div>
-//       </div>
-//     )
-//   }
-
-//   handleWeekendsToggle = () => {
-//     this.setState({
-//       weekendsVisible: !this.state.weekendsVisible
-//     })
-//   }
-
-  
-
-//   handleDateSelect = (selectInfo) => {
-//     this.setState({
-//       modal_state : !this.state.modal_state,
-//       startStr : selectInfo.startStr,
-//       endStr : selectInfo.endStr
-        
-//     })
-
-//     console.log(selectInfo.startStr)
-//     console.log(selectInfo.endStr)
-//     console.log(selectInfo.allDay)
-
-
-//   }
-
-
-
-// //     let title = prompt('가나다라 :')
-// //     // title 값이 있을때, 화면에 calendar.addEvent() json형식으로 일정을 추가
-// //     let calendarApi = selectInfo.view.calendar
-// //     calendarApi.unselect() // clear date selection
-
-    
-    
-
-// //  if (title) {
-// //       calendarApi.addEvent(
-// //         axios
-// //           .post("/cal/insertInfo", {
-// //             id: createEventId(),
-// //             title: title,
-// //             start: selectInfo.startStr,
-// //             end: selectInfo.endStr,
-// //             allDay: selectInfo.allDay,
-// //             // color : InputInfo.select_color
-// //           }, )
-// //           .then(function (res) {
-// //             // 그러면
-// //             console.log(res);
-// //           })
-// //           .catch(function (error) {
-// //             //에러
-// //             console.log("error",error);
-// //           })
-// //       );
-// //       console.log("asds", selectInfo.title);
-// //     }
-// //   };
-  
-
-
-
-
-
-//   handleEventClick = (clickInfo) => {
-//     if (window.confirm(`삭제하시겠습니까? '${clickInfo.event.title}'`)) {
-//       clickInfo.event.remove()
-//     }
-//     console.log("asd",clickInfo);
-//   }
-
-//   handleEvents = (events) => {
-//     this.setState({
-//       currentEvents: events
-//     })
-//   }
-
-// }
-
-
-// function renderEventContent(eventInfo) {
-//   return (
-//     <>
-//       <b>{eventInfo.timeText}</b>
-//       <i>{eventInfo.event.title}</i>
-//     </>
-//   )
-// }
-
-// function renderSidebarEvent(event) {
-//   return (
-//     <li key={event.id}>
-//       <b>{formatDate(event.start, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
-//       <i>{event.title}</i>
-//     </li>
-//   )
-// }
-
--- src/react_project/InputInfo.js
+++ src/react_project/InputInfo.js
@@ -1,183 +1,195 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import Modal from 'react-modal';
-import './InputInfo.css'
+import './InputInfo.css';
 import './Calendar';
 import { createEventId } from './event-utils';
+// import Form from 'react-bootstrap/Form';
 
 function InputInfo(props) {
+  const [text, setText] = useState('');
+  const [part, setPart] = useState('');
+  const [content, setContent] = useState('');
+  const [select_color, setSelect_color] = useState();
+  const [floor, setFloor] = useState('2');
 
-    const [text, setText] = useState("");
-    const [part, setPart] = useState("");
-    const [content, setContent] = useState("");
-    const [select_color, setSelect_color] = useState();
-    const [floor, setFloor] = useState("2");
+  // const [modal_check, setModal_check] = useState(false);
 
-    // const [modal_check, setModal_check] = useState(false);
+  const color_list = [
+    { value: 'null', color: 'Choose Color' },
+    { value: 'red', color: 'Red' },
+    { value: 'blue', color: 'Blue' },
+    { value: 'green', color: 'Green' },
+    { value: 'yellow', color: 'Yellow' },
+    { value: 'orange', color: 'Orange' },
+  ];
 
+  const SelectColor = (props) => {
+    const onSelect = (event) => {
+      setSelect_color(event.target.value);
+    };
+    return (
+      <select onChange={onSelect} value={select_color}>
+        {props.option.map((c) => (
+          <option
+            key={c.value}
+            disabled={c.color === '==Color==' ? true : false}
+            defaultValue={c.color === '==Color==' ? true : false}
+          >
+            {c.color}
+          </option>
+        ))}
+      </select>
+    );
+  };
 
-    const color_list = [
-        {value : 'none', color: 'Choose Color'},
-        {value : 'red', color: 'Red'},
-        {value : 'blue', color: 'Blue'},
-        {value : 'green', color: 'Green'},
-        {value : 'yellow', color: 'Yellow'},
-        {value : 'orange', color: 'Orange'},
-    ]
+  function RadioBtn() {
+    return (
+      <div>
+        <input
+          type='radio'
+          value='2'
+          checked={floor === '2'}
+          onChange={(event) => setFloor(event.target.value)}
+        />
+        <label form='2'>2층</label>
 
+        <input
+          type='radio'
+          value='3'
+          checked={floor === '3'}
+          onChange={(event) => setFloor(event.target.value)}
+        />
+        <label form='3'>3층</label>
+      </div>
+    );
+  }
 
-    const SelectColor = (props) => {
-        const onSelect = (event) => {
-            setSelect_color(event.target.value)
-        }
-        return (
-        <select onChange={onSelect} value={select_color}>
-            {props.option.map((c) => (
-                <option key={c.value} disabled={c.color === '==Color==' ? true : false} defaultValue={c.color === '==Color==' ? true : false}>
-                     {c.color}
-        </option>
-            ))}
-        </select>
-        )
-    }
+  const onClick = (event) => {
+    // event.preventDefault();
 
+    axios
+      .post('http://localhost:8081/api/planSave', {
+        // id : createEventId(),
+        title: text,
+        start_time: props.startStr,
+        end_time: props.endStr,
+        people: part,
+        content: content,
+        bgcolor: select_color,
+        floor: floor,
+      })
+      .then((res) => console.log(res));
 
-    function RadioBtn () {
-        return(
-            <div>
-                    <input type='radio' value="2" 
-                    checked={floor === "2"}
-                    onChange={(event) => setFloor(event.target.value)}/>
-                    <label form='2'>2층</label>
+    let info = {
+      text: text,
+      part: part,
+      content: content,
+      color: select_color,
+      floor: floor,
+      start: props.startStr,
+      end: props.endStr,
+    };
 
-                    <input type='radio' value="3" 
-                    checked={floor === "3"}
-                    onChange={(event) => setFloor(event.target.value)}
-                    />
-                    <label form='3'>3층</label>
-                </div>
-        );
-    }
-    
-    
-    const onClick = (event) => {
-    
-        // event.preventDefault();
-        
-        axios
-        .post('http://localhost:8081/api/planSave',{
-            // id : createEventId(),
-            title : text,
-            start_time : props.startStr,
-            end_time : props.endStr,
-            people : part,
-            content : content,
-            bgcolor : select_color,
-            floor : floor,
+    // axios
+    // .post("http://localhost:3000/rkskek",info)
+    // .then((res) => console.log(res))
 
-        }).then((res) => console.log(res))
-        
-        let info = {
-            text : text,
-            part : part,
-            content : content,
-            color : select_color,
-            floor : floor,
-            start : props.startStr,
-            end : props.endStr,
-        }
+    console.log(info);
 
-        // axios
-        // .post("http://localhost:3000/rkskek",info)
-        // .then((res) => console.log(res))
+    // props.onChange();
+  };
 
-        console.log(info);
+  const modalStyle = {
+    overlay: {
+      position: 'fixed',
+      top: 0,
+      backgroundColor: 'rgba(255, 255, 255, 0.45)',
+      zIndex: 10,
+      justifyContent: 'center',
+      alignItem: 'center',
+    },
+    content: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItem: 'center',
+      background: '#ffffe7',
+      borderRadius: '20px',
+      margin: '0 auto',
+      width: '60%',
+      height: '50%',
 
-        // props.onChange();
+      zIndex: 10,
+    },
+  };
 
-    }
+  return (
+    <div>
+      <Modal
+        isOpen={props.modal_state} //{modal_state} //true시 모달이 나옴 버튼 클릭시 false에서 트루로?
+        style={modalStyle} //모달창 스타일
+        //onRequestClose={false} // 오버레이나 esc를 누르면 isopen값이 false 닫힘
+        ariaHideApp={false}
+      >
+        <form>
+          <div>
+            <SelectColor option={color_list}></SelectColor>
+            <input
+              type='text'
+              placeholder='제목'
+              className='info_head_text'
+              onChange={(event) => setText(event.target.value)}
+              value={text}
+            />
+          </div>
 
-    const modalStyle = {
-        overlay: {
-          position: 'fixed',
-          top: 0,
-          backgroundColor: 'rgba(255, 255, 255, 0.45)',
-          zIndex: 10,
-          justifyContent: 'center',
-          alignItem: 'center',
-        },
-        content: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItem: 'center',
-            background: '#ffffe7',
-            borderRadius: '20px',
-            margin: '0 auto',
-            width: '60%',
-            height: '50%',
-      
-            zIndex: 10,
-          },
-      };
+          <br />
 
-    return(
-        <div>
-            
-            <Modal
-            isOpen={props.modal_state}    //{modal_state} //true시 모달이 나옴 버튼 클릭시 false에서 트루로?
-			style={modalStyle} //모달창 스타일
-			//onRequestClose={false} // 오버레이나 esc를 누르면 isopen값이 false 닫힘
-			ariaHideApp={false}> 
-            
+          <div className='info_body'>
+            <h4 className='info_body_text'>참여자</h4>
+            <input
+              type='text'
+              placeholder='참여자'
+              className='info_partic'
+              onChange={(event) => setPart(event.target.value)}
+            />
+          </div>
 
-            <form className='info_form'>
-                <div>
-                <SelectColor option={color_list}></SelectColor>
-                <input type="text" placeholder="제목" className='info_head_text'
-                onChange = {(event) => setText(event.target.value)}
-                value={text}
-                />
-                </div>
+          <br />
 
-                <br/>
+          <div>
+            <input type='text' defaultValue={props.startStr}></input>
+            <br></br>≀<br></br>
+            {/* <h6>≀</h6> */}
+            <input type='text' defaultValue={props.endStr}></input>
+          </div>
 
-                <div className="info_body">
-                    <h4 className="info_body_text">참여자</h4>
-                    <input type="text" placeholder="참여자" className='info_partic'
-                    onChange={(event) => setPart(event.target.value)}/>
-                </div>
+          <br />
 
-                <br/>
+          <div>
+            <textarea
+              rows='5'
+              cols='60'
+              className='info_content'
+              placeholder='회의내용'
+              onChange={(event) => setContent(event.target.value)}
+            />
+          </div>
 
-                <div>
-                    <input type="text" defaultValue={props.startStr}></input>
-                    <br></br>
-                    ≀
-                    <br></br>
-                    {/* <h6>≀</h6> */}
-                     <input type="text" defaultValue={props.endStr}></input>
-                </div>
+          <div>
+            <button type='submit' onClick={onClick}>
+              저장
+            </button>
+            <button type='button' onClick={props.onChange}>
+              취소
+            </button>
+          </div>
 
-                <br/>
-
-                <div>
-                    <textarea rows="5" cols="60"
-                    className='info_content'
-                    placeholder='회의내용'
-                    onChange={(event) => setContent(event.target.value)}/>
-                </div>
-
-                <div>
-                    <button type="submit" onClick={onClick}>저장</button>
-                    <button type="button" onClick={props.onChange}>취소</button>
-                </div>
-
-                <RadioBtn/>
-                
-            </form>
-            </Modal>
-        </div>
-    )
+          <RadioBtn />
+        </form>
+      </Modal>
+    </div>
+  );
 }
-Modal.setAppElement("#root")
+Modal.setAppElement('#root');
 export default InputInfo;
