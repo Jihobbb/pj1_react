08bbfde61a069d80608d430616a669cc448ffeb3
EHEH <81672416+eunhoya@users.noreply.github.com>
2022-05-09 20:43:29 +0900

    0509
    필터후부터 진행

--- src/react_project/Calendar.jsx
+++ src/react_project/Calendar.jsx
@@ -5,20 +5,37 @@ import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import InputInfo from './InputInfo';
-
 import './Calendar.css';
+import Modify from './Modify';
 
 const Calendar = () => {
   const [modal_state, setModal_state] = useState(false);
+  const [modify_state, setModify_state] = useState(false);
   const [startStr, setStartStr] = useState('');
   const [endStr, setEndStr] = useState('');
   const [user, setUser] = useState([]);
 
-  const getdata = async () => {
-    const response = await axios.get('http://localhost:8081//api/planDelete/');
+  const [m_title, setM_title] = useState();
+  const [m_people, setM_people] = useState();
+  const [m_content, setM_content] = useState();
+  const [m_bgcolor, setM_bgcolo] = useState();
+  const [m_id, setM_id] = useState();
+  const [m_floor, setM_floor] = useState();
 
+  const [togleBtn, setTogleBtn] = useState('2');
+
+  const getdata = async () => {
+    const response = await axios.get('http://localhost:8081/api/planList');
     setUser(response.data);
+    const floor = response.data.filter(function (element) {
+      return element.floor === togleBtn;
+    });
+    console.log('floor층수', floor);
   };
+
+  useEffect(() => {
+    getdata();
+  }, [togleBtn]);
 
   useEffect(() => {
     getdata();
@@ -34,21 +51,41 @@ const Calendar = () => {
     setModal_state(!modal_state);
   };
 
-  const handleEventClick = (clickInfo) => {
-    axios
-      .delete('http://localhost:8081/api/planDelete/{id}')
-      .then((response) => {
-        console.log('삭제 요청 성공');
-        if (window.confirm(`삭제 '${clickInfo.event.title}'`)) {
-          clickInfo.event.remove();
-        }
-      })
-      .catch(() => {
-        console.log('삭제 요청 실패');
-      });
-
-    console.log('handleEventClick', clickInfo);
+  const onChange_M = () => {
+    setModify_state(!modify_state);
   };
+
+  // const infoDelete = (clickInfo) => {
+  //   if(window.confirm(`삭제 '${clickInfo.event.title}'`)){
+
+  //     axios.delete(`http://localhost:8081/api/planDelete/${clickInfo.event.id}`)
+  //   }
+  //     window.location.replace("/")
+  //   }
+
+  const handleEventClick = (clickInfo) => {
+    // <Modify title={clickInfo.event.title} start_time={clickInfo.event.start_time} end_time={clickInfo.event.end_time} people={clickInfo.event.people} content={clickInfo.event.content} bgcolor={clickInfo.event.bgcolor} floor={clickInfo.event.floor}/>
+    setModify_state(!modal_state);
+    setM_id(clickInfo.event.id);
+    setM_title(clickInfo.event.title);
+    setM_people(clickInfo.event.people);
+    setM_bgcolo(clickInfo.event.bgcolor);
+    setM_content(clickInfo.event.content);
+    setM_floor(clickInfo.event.floor);
+  };
+  console.log(m_people);
+
+  // if (window.confirm(`삭제 '${clickInfo.event.title}'`)) {
+  //   axios
+  //     .delete(`http://localhost:8081/api/planDelete/${clickInfo.event.id}`)
+  //     .then(function (res) {
+  //       window.location.replace("/")
+  //     })
+  //     .catch(function (error) {
+  //       console.log('delErr', error);
+  //     });
+  // }
+  // console.log('handleEventClick', clickInfo);
 
   function renderEventContent(eventInfo) {
     return (
@@ -59,16 +96,27 @@ const Calendar = () => {
     );
   }
 
-  // const box = () => {
-  //   user.map(user => {
-  //     title = user.title,
-  //     start = user.start_time,
-  //     end = user.end_time
-  //   })
-  // }
-
   return (
     <div>
+      <input
+        type='radio'
+        value='2'
+        checked={togleBtn === '2'}
+        onChange={(event) => setTogleBtn(event.target.value)}
+      />
+
+      <label form='2'>2층</label>
+
+      <input
+        type='radio'
+        value='3'
+        checked={togleBtn === '3'}
+        onChange={(event) => {
+          setTogleBtn(event.target.value);
+          console.log('??');
+        }}
+      />
+      <label form='3'>3층</label>
       <InputInfo
         modal_state={modal_state}
         startStr={startStr}
@@ -76,11 +124,21 @@ const Calendar = () => {
         onChange={onChange}
       />
 
+      <Modify
+        modify_state={modify_state}
+        onChange={onChange_M}
+        id={m_id}
+        title={m_title}
+        people={m_people}
+        content={m_content}
+        bgcolor={m_bgcolor}
+        floor={m_floor}
+      />
+
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         locale='ko'
         businessHours={true} // 주말 색깔 블러 처리
-        weekends={false} //주말 볼지 말지
         headerToolbar={{
           left: 'prev',
           center: 'title',
@@ -89,11 +147,15 @@ const Calendar = () => {
         initialView='timeGridWeek'
         ///////////////////////////////
         events={user.map((user) => ({
+          people: user.people,
+          id: user.id,
           title: user.title,
           start: user.start_time,
           end: user.end_time,
+          backgroundColor: user.bgcolor,
+          borderColor: user.bgcolor,
         }))}
-        ////////////////////////////////
+        ///////////////////////////////
 
         // events = {{title : 'All Day',
         //         start: '2022-05-10'}}
@@ -107,13 +169,13 @@ const Calendar = () => {
 
         eventClick={handleEventClick}
         select={handleDateSelect}
-        editable={true} // 수정 ?
+        editable={false} // 수정 ?
         selectable={true} //드래그 가능
         selectMirror={true}
         eventContent={renderEventContent}
         // 이벤트명 : function(){} : 각 날짜에 대한 이벤트를 통해 처리할 내용..
         dayMaxEvents={true}
-        weekends={false} //주말
+        weekends={false} //주말 볼지 말지
       />
     </div>
   );
