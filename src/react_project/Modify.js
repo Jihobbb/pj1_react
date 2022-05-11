77cca48cea4361ed365c279a843ec0f32f0087cf
EHEH <81672416+eunhoya@users.noreply.github.com>
2022-05-11 18:17:39 +0900

    모달 css
    바?꿀까

--- src/react_project/Modify.js
+++ src/react_project/Modify.js
@@ -1,143 +1,155 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
-import './InputInfo.css'
-import './Calendar';
+import './InputInfo.css';
+import './Calendar.css';
 
 function Modify(props) {
-    
-    const [updateData, setData] = useState({
-        title: "",
-        people: "",
-        content: "",
+  const [updateData, setData] = useState({
+    title: '',
+    people: '',
+    content: '',
+  });
+
+  const setDefaultData = () => {
+    console.log(props.start);
+    setData({
+      title: props.plan.title,
+      people: props.plan.people,
+      content: props.plan.content,
     });
+  };
 
-    const setDefaultData = () => {
-        console.log(props.start)
-        setData({
-            title: props.plan.title,
-            people: props.plan.people,
-            content: props.plan.content
+  useEffect(() => {
+    setDefaultData();
+  }, [props]);
+
+  const asd = () => {
+    axios
+      .put('http://localhost:8081/api/planUpdate', {
+        id: props.plan.id,
+        title: updateData.title,
+        start_time: props.start,
+        end_time: props.end,
+        people: updateData.people,
+        content: updateData.content,
+        bgcolor: props.plan.bgcolor,
+        floor: props.plan.floor,
+      })
+      .then((res) => console.log(res));
+  };
+
+  const delete_btn = () => {
+    if (window.confirm(`삭제 '${props.plan.title}'`)) {
+      axios
+        .delete(`http://localhost:8081/api/planDelete/${props.plan.id}`)
+        .then(function (res) {
+          window.location.replace('/');
         })
+        .catch(function (error) {
+          console.log('delErr', error);
+        });
     }
+  };
 
-    useEffect(() => {
-        setDefaultData();
-      }, [props]);
+  const modalStyle = {
+    overlay: {
+      position: 'fixed',
+      top: 0,
+      backgroundColor: 'rgba(255, 255, 255, 0.45)',
+      zIndex: 10,
+      justifyContent: 'center',
+      alignItem: 'center',
+    },
+    content: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItem: 'center',
+      background: '#ffffe7',
+      borderRadius: '20px',
+      margin: '0 auto',
+      width: '60%',
+      height: '50%',
 
-    
-    const asd = () => {
-            
-        axios
-        .put('http://localhost:8081/api/planUpdate',{
-            id : props.plan.id,
-            title : updateData.title,
-            start_time : props.start,
-            end_time : props.end,
-            people : updateData.people,
-            content : updateData.content,
-            bgcolor : props.plan.bgcolor,
-            floor : props.plan.floor,
+      zIndex: 10,
+    },
+  };
 
-        }).then((res) => console.log(res))
-    }
-
-    const delete_btn = () => {
-        if (window.confirm(`삭제 '${props.plan.title}'`)) {
-              axios
-                .delete(`http://localhost:8081/api/planDelete/${props.plan.id}`)
-                .then(function (res) {
-                  window.location.replace("/")
-                })
-                .catch(function (error) {
-                  console.log('delErr', error);
+  return (
+    <div>
+      <Modal
+        isOpen={props.modify_state} //{modal_state} //true시 모달이 나옴 버튼 클릭시 false에서 트루로?
+        style={modalStyle} //모달창 스타일
+        //onRequestClose={false} // 오버레이나 esc를 누르면 isopen값이 false 닫힘
+        ariaHideApp={false}
+      >
+        <form className='info_form' onSubmit={asd}>
+          <div>
+            <input
+              type='text'
+              value={updateData.title}
+              className='info_head_text'
+              onChange={(e) => {
+                setData({
+                  ...updateData,
+                  title: e.target.value,
                 });
-            }
-    }
+              }}
+            />
+          </div>
 
-    const modalStyle = {
-        overlay: {
-          position: 'fixed',
-          top: 0,
-          backgroundColor: 'rgba(255, 255, 255, 0.45)',
-          zIndex: 10,
-          justifyContent: 'center',
-          alignItem: 'center',
-        },
-        content: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItem: 'center',
-            background: '#ffffe7',
-            borderRadius: '20px',
-            margin: '0 auto',
-            width: '60%',
-            height: '50%',
-      
-            zIndex: 10,
-          },
-      };
+          <br />
 
-    return(
-        <div>
-            
-            <Modal
-            isOpen={props.modify_state}    //{modal_state} //true시 모달이 나옴 버튼 클릭시 false에서 트루로?
-			style={modalStyle} //모달창 스타일
-			//onRequestClose={false} // 오버레이나 esc를 누르면 isopen값이 false 닫힘
-			ariaHideApp={false}> 
-            
+          <div className='info_body'>
+            <h4 className='info_body_text'>참여자</h4>
+            <input
+              type='text'
+              value={updateData.people}
+              className='info_partic'
+              onChange={(e) => {
+                setData({
+                  ...updateData,
+                  people: e.target.value,
+                });
+              }}
+            />
+          </div>
 
-            <form className='info_form' onSubmit={asd}>
-                <div>
-                <input type="text" value={updateData.title} className='info_head_text'
-                onChange = {(e) =>{
-                        setData({
-                            ...updateData, title: e.target.value
-                        })
-                }}/>
-                </div>
+          <br />
 
-                <br/>
+          <br />
 
-                <div className="info_body">
-                    <h4 className="info_body_text">참여자</h4>
-                    <input type="text" value={updateData.people} className='info_partic'
-                    onChange = {(e) =>{
-                        setData({
-                            ...updateData, people: e.target.value
-                        })
-                }}/>
-                </div>
+          <div>
+            <textarea
+              rows='5'
+              cols='60'
+              className='info_content'
+              value={updateData.content}
+              onChange={(e) => {
+                setData({
+                  ...updateData,
+                  content: e.target.value,
+                });
+              }}
+            />
+          </div>
 
-                <br/>
+          <br />
 
-                <br/>
-
-                <div>
-                    <textarea rows="5" cols="60"
-                    className='info_content'
-                    value={updateData.content}
-                    onChange = {(e) =>{
-                        setData({
-                            ...updateData, content: e.target.value
-                        })
-                }}/>
-                </div>
-
-                <br/>
-
-                <div>
-                <button type='submit'>수정</button>
-                <button type='button' onClick={delete_btn}>삭제</button>
-                <button type="button" onClick={props.onChange}>취소</button>
-                </div>
-                
-            </form>
-            </Modal>
-        </div>
-    )
+          <div>
+            <button type='submit'>수정</button>
+            <button type='button' onClick={delete_btn}>
+              삭제
+            </button>
+            <button type='button' onClick={props.onChange}>
+              취소
+            </button>
+          </div>
+        </form>
+      </Modal>
+    </div>
+  );
 }
-Modal.setAppElement("#root")
+Modal.setAppElement('#root');
 
 export default Modify;
