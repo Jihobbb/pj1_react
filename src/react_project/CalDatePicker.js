cda042e6274c27ee8e1477c8e850368f37cc778b
EHEH <81672416+eunhoya@users.noreply.github.com>
2022-05-20 16:51:45 +0900

    피커 위치
    ㅇㅇ

--- src/react_project/CalDatePicker.js
+++ src/react_project/CalDatePicker.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import {ko} from 'date-fns/esm/locale'
+import './CalDatePicker.css';
+import { ko } from 'date-fns/esm/locale';
 
 const CalDatePicker = (props) => {
   return (
-    <DatePicker    
-    onChange={(date) =>{
-      const calendarApi = props.calendarRef.current.getApi();
-      calendarApi.gotoDate(date);
-    }}
-    dateFormat='yyyy-MM-dd'  
-    locale={ko}  
-    inline 
+    <DatePicker
+      onChange={(date) => {
+        const calendarApi = props.calendarRef.current.getApi();
+        calendarApi.gotoDate(date);
+      }}
+      dateFormat='yyyy-MM-dd'
+      locale={ko}
+      inline
     />
   );
 };
--- src/react_project/Planner.jsx
+++ src/react_project/Planner.jsx
@@ -5,7 +5,7 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
-import CalDatePicker from './CalDatePicker'
+import CalDatePicker from './CalDatePicker';
 
 const Planner = (props) => {
   const [floorStatus, setFloorStatus] = useState('2');
@@ -16,8 +16,7 @@ const Planner = (props) => {
   //datepicker 온오프
   const handlePickerClick = (e) => {
     setisDatePickerOpen(!isDatePickerOpen);
-  }
-
+  };
 
   //드래그해서 기간설정
   const handleDateSelect = (selectInfo) => {
@@ -84,85 +83,87 @@ const Planner = (props) => {
   };
 
   return (
-  <div>
-    {isDatePickerOpen && <CalDatePicker
-      calendarRef = {calendarComponentRef}
-      isopen = {handlePickerClick}
-    />}
-    <FullCalendar
-      plugins={[
-        dayGridPlugin,
-        timeGridPlugin,
-        interactionPlugin,
-        listPlugin,
-        googleCalendarPlugin,
-      ]}
-      ref={calendarComponentRef}
-      googleCalendarApiKey={'AIzaSyDuIfK2-Xqvji3V8FC8q9mlXVdX5kYmNEo'}
-      locale='ko'
-      CustomButtons
-      customButtons={{
-        floor2F: {
-          text: '2층',
-          click: function () {
-            setFloorStatus('2');
-            props.floorChange('2');
-            props.refresh();
+    <div>
+      <div className='datepickerBox'>
+        {isDatePickerOpen && (
+          <CalDatePicker
+            calendarRef={calendarComponentRef}
+            isopen={handlePickerClick}
+          />
+        )}
+      </div>
+      <FullCalendar
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          listPlugin,
+          googleCalendarPlugin,
+        ]}
+        ref={calendarComponentRef}
+        googleCalendarApiKey={'AIzaSyDuIfK2-Xqvji3V8FC8q9mlXVdX5kYmNEo'}
+        locale='ko'
+        CustomButtons
+        customButtons={{
+          floor2F: {
+            text: '2층',
+            click: function () {
+              setFloorStatus('2');
+              props.floorChange('2');
+              props.refresh();
+            },
           },
-        },
-        floor3F: {
-          text: '3층',
-          click: function () {
-            setFloorStatus('3');
-            props.floorChange('3');
-            props.refresh();
+          floor3F: {
+            text: '3층',
+            click: function () {
+              setFloorStatus('3');
+              props.floorChange('3');
+              props.refresh();
+            },
           },
-        },
-        togleWeekend: {
-          text: '주말보기',
-          click() {
-            setWeekendActive(!weekendActive);
-          }
-        },
-        moveDate: {
-          text: '선택 ∇',
-          click() {
-            handlePickerClick();
-          }
-        }
-      }}
-      headerToolbar={{
-        left: 'prev today next togleWeekend moveDate',
-        center: 'title',
-        right: 'floor2F,floor3F dayGridMonth,timeGridWeek,listWeek',
-      }}
-      initialView='timeGridWeek'
-      //------------이벤트 리스트 정의---------------
-      eventSources={[planListMapping(), holidayListMapping()]}
-
-      //------------설정 값 정리---------------
-      eventClick={handleEventClick}
-      select={handleDateSelect}
-      selectable={true} //드래그 가능 여부
-      selectMirror={true}
-      slotMinTime={'07:00:00'} //시간 범위 설정
-      slotMaxTime={'20:00:00'}
-      expandRows={true}
-      dayMaxEvents={6}
-      weekends={weekendActive}
-      eventOverlap={false} //이벤트 겹쳐지기 막음
-      selectOverlap={false} //등록시에도 겹쳐지지 않음
-      dayMaxEventRows={true}
-
-      //------------드래깅으로 수정--------------
-      editable={true} // 수정 가능
-      eventStartEditable={true}
-      eventResizableFromStart={true}
-      droppable={true}
-      eventDrop={dragAnddrop} //일정 옮겨서 떨어뜨릴 때 발생
-      eventResize={eventSizing} //일정을 크기조절하여 기간 변경 시 발생
-    />
-  </div>
+          togleWeekend: {
+            text: '주말보기',
+            click() {
+              setWeekendActive(!weekendActive);
+            },
+          },
+          moveDate: {
+            text: '선택 ∇',
+            click() {
+              handlePickerClick();
+            },
+          },
+        }}
+        headerToolbar={{
+          left: 'prev today next togleWeekend moveDate',
+          center: 'title',
+          right: 'floor2F,floor3F dayGridMonth,timeGridWeek,listWeek',
+        }}
+        initialView='timeGridWeek'
+        //------------이벤트 리스트 정의---------------
+        eventSources={[planListMapping(), holidayListMapping()]}
+        //------------설정 값 정리---------------
+        eventClick={handleEventClick}
+        select={handleDateSelect}
+        selectable={true} //드래그 가능 여부
+        selectMirror={true}
+        slotMinTime={'07:00:00'} //시간 범위 설정
+        slotMaxTime={'20:00:00'}
+        expandRows={true}
+        dayMaxEvents={6}
+        weekends={weekendActive}
+        eventOverlap={false} //이벤트 겹쳐지기 막음
+        selectOverlap={false} //등록시에도 겹쳐지지 않음
+        dayMaxEventRows={true}
+        //------------드래깅으로 수정--------------
+        editable={true} // 수정 가능
+        eventStartEditable={true}
+        eventResizableFromStart={true}
+        droppable={true}
+        eventDrop={dragAnddrop} //일정 옮겨서 떨어뜨릴 때 발생
+        eventResize={eventSizing} //일정을 크기조절하여 기간 변경 시 발생
+      />
+    </div>
   );
 };
 
